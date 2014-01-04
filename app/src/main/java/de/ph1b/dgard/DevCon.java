a715be5de44831cd39d52c6a69973d458a0e768f
ph1b <woitaschek@gmail.com>
2014-01-04 17:04:59 +0100

    updated login and numberformat, +gradle changes

--- app/src/main/java/de/ph1b/dgard/DevCon.java
+++ app/src/main/java/de/ph1b/dgard/DevCon.java
@@ -1,18 +1,10 @@
 package de.ph1b.dgard;
 
 
-import java.io.IOException;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-
-import de.ub0r.android.websms.connector.common.Connector;
-import de.ub0r.android.websms.connector.common.ConnectorCommand;
-import de.ub0r.android.websms.connector.common.ConnectorSpec;
-import de.ub0r.android.websms.connector.common.ConnectorSpec.SubConnectorSpec;
-import de.ub0r.android.websms.connector.common.Log;
 
 import com.telekom.api.common.ServiceEnvironment;
 import com.telekom.api.common.auth.TelekomOAuth2Auth;
@@ -23,13 +15,22 @@ import com.telekom.api.sendsms.OutboundSMSType;
 import com.telekom.api.sendsms.SendSmsClient;
 import com.telekom.api.sendsms.model.SendSmsRequest;
 
+import java.io.IOException;
+
+import de.ub0r.android.websms.connector.common.Connector;
+import de.ub0r.android.websms.connector.common.ConnectorCommand;
+import de.ub0r.android.websms.connector.common.ConnectorSpec;
+import de.ub0r.android.websms.connector.common.ConnectorSpec.SubConnectorSpec;
+import de.ub0r.android.websms.connector.common.Log;
+import de.ub0r.android.websms.connector.common.Utils;
+import de.ub0r.android.websms.connector.common.WebSMSException;
+
 
 public class DevCon extends Connector {
 
     private static final String TAG = "Developergarden";
     static String subAccountId = null;
     static String scope = "DC0QX4UK";
-    TelekomOAuth2Auth auth;
 
     private static void outputErrorAndAbort(SmsResponse response) {
         if (response.getRequestError().getServiceException() != null) {
@@ -81,21 +82,22 @@ public class DevCon extends Connector {
         return connectorSpec;
     }
 
-    private boolean login(final Context context)
+    //initiates login and returns authentification
+    private TelekomOAuth2Auth login(final Context context)
             throws IOException {
         SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(context);
         String token = p.getString(Preferences.PREFS_PASSWORD, "");
         String clientId = p.getString(Preferences.PREFS_CLIENTID, "");
 
-        auth = new TelekomOAuth2Auth(clientId, token, scope);
+        TelekomOAuth2Auth auth = new TelekomOAuth2Auth(clientId, token, scope);
         auth.requestAccessToken();
         if (!auth.hasValidToken())
-            Log.e(TAG, "Authentication error");
-
-        return true;
+            throw new WebSMSException("No valid token!");
+        return auth;
     }
 
-    private void sendMessage(final Context context, final Intent intent) {
+    // sending actual message using the the auth provied by login()
+    private void sendMessage(final Context context, final Intent intent, TelekomOAuth2Auth auth) {
         ConnectorCommand localConnectorCommand = new ConnectorCommand(intent);
         SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(context);
         String senderNumber = p.getString(Preferences.PREFS_CUSTOMNUMBER, "");
@@ -103,10 +105,11 @@ public class DevCon extends Connector {
         SendSmsRequest request = new SendSmsRequest();
         String text = localConnectorCommand.getText();
 
+        //reformats
         String[] reci = localConnectorCommand.getRecipients();
         String[] reciFormat = new String[reci.length];
         for (int i = 0; i < reci.length; i++) {
-            reciFormat[i] = "tel:" + reci[i].substring(reci[i].indexOf("<") + 1, reci[i].indexOf(">"));
+            reciFormat[i] = "tel:" + Utils.national2international(localConnectorCommand.getDefPrefix(), Utils.getRecipientsNumber(reci[i]));
             Log.i(TAG, "Sending to: " + reciFormat[i]);
         }
 
@@ -127,20 +130,32 @@ public class DevCon extends Connector {
 
     }
 
+    //gets free sms available and sets them to connector spec
     @Override
     protected final void doUpdate(final Context context, final Intent intent)
             throws IOException {
-        login(context);
+        TelekomOAuth2Auth auth = login(context);
+        ConnectorSpec c = getSpec(context);
         QuotaClient client = new QuotaClient(auth, ServiceEnvironment.SANDBOX);
         GetQuotaInformationResponse quotaResponse = client.getQuotaInformation("GlobalSmsSandbox");
-        getSpec(context).setBalance(String.valueOf(quotaResponse.getMaxQuota()-quotaResponse.getQuotaLevel()));
+        Log.i(TAG, "Free SMS: " + String.valueOf(quotaResponse.getMaxQuota() - quotaResponse.getQuotaLevel()));
+        c.setBalance(String.valueOf(quotaResponse.getMaxQuota() - quotaResponse.getQuotaLevel()));
     }
 
+    //initiates login, then checks for free then initiates sending
     @Override
     protected final void doSend(final Context context, final Intent intent)
             throws IOException {
-        this.login(context);
-        this.sendMessage(context, intent);
+        TelekomOAuth2Auth auth = this.login(context);
+        doUpdate(context, intent);
+
+        /* temporary disabled because of buggy update
+        if (getSpec(context).getBalance().equals("0")) {
+            throw new WebSMSException(context, R.string.no_free_sms);
+        }*/
+
+        this.sendMessage(context, intent, auth);
     }
+
 
 }
